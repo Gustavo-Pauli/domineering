db852584a657155a8bfd20a41e7855d62d58be53
Gustavo Pauli <68567054+Gustavo-Pauli@users.noreply.github.com>
2025-03-30 21:58:21 -0300

    first commit

---
+++ src/player_interface.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+class PlayerInterface:
+  def __init__(self):
+    self.main_window = tk.Tk()  # instanciar Tk (que implementa a janela)
+    self.main_window.mainloop()  # abrir a janela
